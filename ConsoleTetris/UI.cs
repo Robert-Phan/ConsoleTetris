9b1d3c94f93ceaa0b7cd553b0907d7f6194f1547
Robert-Phan <fannk987@gmail.com>
2022-06-29 10:06:42 +0700

    Added help menu.

--- ConsoleTetris/UI.cs
+++ ConsoleTetris/UI.cs
@@ -117,7 +117,7 @@ class Selection
 		Offset = offset;
 	}
 
-	internal string Start()
+	internal string? Start()
 	{
 		var (x, y) = Offset;
 
@@ -170,6 +170,11 @@ class Selection
 				Index = 0;
 				return SelectionsArray[i];
 			}
+			else if (key == ConsoleKey.X)
+			{
+				Console.ResetColor();
+				return null;
+			}
 		}
 	}
 }
@@ -191,13 +196,24 @@ static internal class UI
 		{"Enable Drop Projection", "True" }
 	}, (20, 7));
 
+	static Selection HelpScreen { get; } = new(new[]
+	{
+		"Gameplay Controls",
+		"Gameplay Content",
+		"Gameplay Settings"
+	}, (22, 7));
+
 	internal static void Start()
 	{
 		Console.CursorVisible = false;
 		Console.Clear();
+		if (OperatingSystem.IsWindows())
+			Console.SetWindowSize(60, 30);
 		DisplayLogo();
 
 		var selection = StartScreen.Start();
+
+		if (selection == null) return;
 		if (selection == "Play")
 		{
 			Game.Start();
@@ -211,7 +227,12 @@ static internal class UI
 			ParseSettings(settings);
 			Start();
 		}
-		else if (selection == "Help") Start();
+		else if (selection == "Help")
+		{
+			DisplayHelp();
+			Console.Clear();
+			Start();
+		};
 	}
 
 	static void ParseSettings(Dictionary<string, string> settings)
@@ -240,6 +261,84 @@ static internal class UI
 			.Any(ch => settings["Enable Drop Projection"].Contains(ch));
 	}
 
+	static string GameplayControls { get; } = @"
+GAMEPLAY CONTROLS
+
+Left Arrow: Move Piece Left
+Right Arrow: Move Piece Right
+'A' key: Rotate Left
+'D' key: Rotate Right
+Down Arrow: Move Piece Down
+Up Arrow: Instant Drop
+'X' key: Return To Title Screen
+";
+
+	static string GameplayContent { get; } = @"
+GAMEPLAY CONTENT
+
+Tetris pieces spawn in upper two rows (indicated by red exclamation marks (!)).
+The player fails the game when a piece is dropped inside those two rows.
+
+Tetris pieces on the board project themselves down onto the floor/other blocks.
+You can press the Up Arrow key to instantly drop the piece down.
+
+The 'NEXT' box shows the piece that will appear 
+after the current piece has been dropped.
+
+The 'SCORE' box shows the player's points accumulated through destroying lines.
+The amount of points earned depends on the amount of lines destroyed at once:
+1 line : 1000  points
+2 lines: 6000  points
+3 lines: 16000 points
+4 lines: 32000 points
+";
+
+	static string GameplaySettings { get; } = @"
+GAMEPLAY SETTINGS
+
+'Width': The width of the gameplay board, in blocks
+'Height': The height of the gameplay board, in blocks
+'Fall Time': How fast pieces naturally fall, in milliseconds.
+'Enable Drop Projection': Whether the player can see a piece's drop projection or not.
+";
+
+	static void DisplayHelp()
+	{
+		Console.Clear();
+		var helpSelection = HelpScreen.Start();
+
+		if (helpSelection == null) return;
+		else
+		{
+			Console.Clear();
+
+			var helpMessage = 
+				(helpSelection == "Gameplay Controls" ? GameplayControls :
+				helpSelection == "Gameplay Content" ? GameplayContent :
+				GameplaySettings).Split('\n');
+
+			if (OperatingSystem.IsWindows())
+			{
+				var longestLine = helpMessage
+					.OrderByDescending(line => line.Length)
+					.First();
+				Console.WindowWidth = 19 + longestLine.Length;
+			}
+
+			for (int i = 1; i < helpMessage.Length; i++)
+			{
+				Console.SetCursorPosition(10, 6 + i);
+				Console.WriteLine(helpMessage[i]);
+			}
+
+			Console.ReadKey();
+			if (OperatingSystem.IsWindows())
+				Console.WindowWidth = 60;
+
+			DisplayHelp();
+		}
+	}
+
 	static string Logo { get; } = @" 
  _____  _____  _____  ____   ___  ____  
 |_   _|| ____||_   _||  _ \ |_ _|/ ___| 
@@ -250,9 +349,6 @@ static internal class UI
 
 	static void DisplayLogo()
 	{
-		if (OperatingSystem.IsWindows())
-			Console.WindowWidth = 60;
-
 		var logoByLine = Logo.Split('\n');
 		for (int i = 0; i < logoByLine.Length; i++)
 		{
